d02fd08d75c27dcf007dfd419390a35185b63e0d
gabdotcom <gabrieldv412@gmail.com>
2025-02-23 23:39:22 +0800

    3rd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card'
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
+import { HttpClientModule } from '@angular/common/http';
 
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
@@ -40,7 +41,8 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     MatFormFieldModule,
     MatCardModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    HttpClientModule
 
   ],
   providers: [],
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,24 +1,21 @@
-import { Component, Input, OnDestroy, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { Subscription } from "rxjs";
-import { Post } from "../posts.model"
-import { PostsService } from '../posts.service';
+import { Post } from "../posts.model";
+import { PostsService } from "../posts.service";
 
 @Component({
-    selector: 'app-post-list',
-    templateUrl: './post-list.component.html',
-    styleUrls: ['./post-list.component.css']
+    selector: "app-post-list",
+    templateUrl: "./post-list.component.html",
+    styleUrls: ["./post-list.component.css"],
 })
-
 export class PostListComponent implements OnInit, OnDestroy {
-
     posts: Post[] = [];
     private postsSub!: Subscription;
 
+    constructor(public postsService: PostsService) { }
 
-    constructor(public postsService: PostsService) {
-    }
     ngOnInit() {
-        this.posts = this.postsService.getPosts();
+        this.postsService.getPosts();  // Fetch posts without assignment
         this.postsSub = this.postsService.getPostUpdatedListener()
             .subscribe((posts: Post[]) => {
                 this.posts = posts;
@@ -28,5 +25,4 @@ export class PostListComponent implements OnInit, OnDestroy {
     ngOnDestroy() {
         this.postsSub.unsubscribe();
     }
-
 }
